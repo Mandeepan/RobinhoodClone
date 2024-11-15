837350658f645a78f1ce331f3a476c4394114cea
QuentinPace <quentinepace@gmail.com>
2024-11-15 13:49:10 -0500

    more robinhood clone styles

--- react-vite/src/components/SignupFormPage/SignupFormPage.jsx
+++ react-vite/src/components/SignupFormPage/SignupFormPage.jsx
@@ -42,57 +42,60 @@ function SignupFormPage() {
   };
 
   return (
-    <div className='signup-page'>
-      <div className='signup-section'>
-        <h1 className='signup-title'>Sign Up</h1>
-        {errors.server && <p className='error'>{errors.server}</p>}
-        <form className='signup-form' onSubmit={handleSubmit}>
-          <label>
-            Email
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </label>
-          {errors.email && <p className='error'>{errors.email}</p>}
-          <label>
-            Username
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </label>
-          {errors.username && <p className='error'>{errors.username}</p>}
-          <label>
-            Password
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-          {errors.password && <p className='error'>{errors.password}</p>}
-          <label>
-            Confirm Password
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-          </label>
-          {errors.confirmPassword && <p className='error'>{errors.confirmPassword}</p>}
-          <button className="sign-up-button" type="submit">Sign Up</button>
-          <button className= 'login-button' 
-          onClick={()=> navigate('/login')}>Log In</button>
-        </form>
+    <div className="signup-page-container">
+        <div className='signup-page'>
+        </div>
+          <div className="signup-section-container">
+            <div className='signup-section'>
+              <h1 className='signup-title'>Sign Up</h1>
+              {errors.server && <p className='error'>{errors.server}</p>}
+              <form className='signup-form' onSubmit={handleSubmit}>
+                <label>
+                  Email
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </label>
+                {errors.email && <p className='error'>{errors.email}</p>}
+                <label>
+                  Username
+                  <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </label>
+                {errors.username && <p className='error'>{errors.username}</p>}
+                <label>
+                  Password
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </label>
+                {errors.password && <p className='error'>{errors.password}</p>}
+                <label>
+                  Confirm Password
+                  <input
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                  />
+                </label>
+                {errors.confirmPassword && <p className='error'>{errors.confirmPassword}</p>}
+                <button className="sign-up-button" type="submit">Sign Up</button>
+                <button className= 'login-button' 
+                onClick={()=> navigate('/login')}>Log In</button>
+              </form>
+          </div>
       </div>
-      
     </div>
   );
 }
